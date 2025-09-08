2ac567785b32b94ecb1299cd0d6f939732e0fe66
FrankoLiamJBetarmos <frankoliam.betarmos@smc.pshs.edu.ph>
2025-09-08 11:05:35 +0800

    Add files via upload

---
+++ SG6-Code2.py
@@ -0,0 +1,5 @@
+import math
+radius = input("Please enter the radius:")
+rad_num = float(radius)
+Area = math.pi * rad_num * rad_num
+print(f"The area of a circle with a radius of {radius} is: {Area:.2f}")
