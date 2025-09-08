64931aec9fe681db337654c0356a80139339d5d8
FrankoLiamJBetarmos <frankoliam.betarmos@smc.pshs.edu.ph>
2025-09-08 10:39:27 +0800

    Add files via upload

---
+++ SG6 -Code1.py
@@ -0,0 +1,8 @@
+user_input = input("Enter the first number (can include decimal points): ")
+num_1 = float(user_input)
+user_inpur = input("Enter the second number (can include decimal points): ")
+num_2 = float(user_inpur)
+user_inpud = input("Enter the third number (can include decimal points_: ")
+num_3 = float(user_inpud)
+sum = num_1 + num_2 + num_3
+print("The total of the three numbers is:" + str(sum))
